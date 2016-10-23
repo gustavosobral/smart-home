2c21a675ab78b72ca8393d7a95b05d284c95c035
Gustavo Sobral <ghsobral@gmail.com>
2016-10-23 14:31:22 -0300

    Setup leaf node

---
+++ leaf-node/leaf-node.ino
@@ -0,0 +1,14 @@
+#include <SPI.h>
+#include <RF24_config.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
